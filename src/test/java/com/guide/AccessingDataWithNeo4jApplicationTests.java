3e5ba3a1f9529001dfe8a494db47f794d8bbd77f
simsonmoses <sims91502@gmail.com>
2023-09-12 16:06:55 +0530

    added neo4j

---
+++ src/test/java/com/guide/AccessingDataWithNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataWithNeo4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
