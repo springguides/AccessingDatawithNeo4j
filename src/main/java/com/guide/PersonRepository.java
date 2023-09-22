084a7ccc5c4c070b33b9ae7789dd6eb9e868bcc6
simsonmoses <sims91502@gmail.com>
2023-09-22 07:06:24 +0530

    added the @repo annotation

--- src/main/java/com/guide/PersonRepository.java
+++ src/main/java/com/guide/PersonRepository.java
@@ -1,9 +1,11 @@
 package com.guide;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PersonRepository extends Neo4jRepository {
     Person findByName(String name);
     List<Person> findByTeammatesName(String name);
